f3a161d2727fcee3104cb71fe89ec6f2381bef38
jonzappola <55258519+jonzappola@users.noreply.github.com>
2023-10-04 13:05:43 -0400

    routes

---
+++ src/server/routes/auth.js
@@ -0,0 +1,13 @@
+// auth.js
+
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+
+// User registration
+router.post('/register', authController.register);
+
+// User login
+router.post('/login', authController.login);
+
+module.exports = router;
