294049ddf51a23806648dbd05f624a1705c46ffb
jonzappola <55258519+jonzappola@users.noreply.github.com>
2023-10-04 13:03:59 -0400

    Add files via upload

---
+++ src/server/controllers/productController.js
@@ -0,0 +1,88 @@
+const Product = require('../models/product');
+
+// List all products
+exports.listProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+// Create a new product
+exports.createProduct = async (req, res) => {
+  try {
+    const { name, description, price } = req.body;
+    
+    // Validate input data (you can add more validation as needed)
+    if (!name || !description || !price) {
+      return res.status(400).json({ error: 'Name, description, and price are required' });
+    }
+
+    const newProduct = new Product({ name, description, price });
+    const savedProduct = await newProduct.save();
+    res.json(savedProduct);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+// Get details of a specific product
+exports.getProduct = async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+// Update product details
+exports.updateProduct = async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const { name, description, price } = req.body;
+
+    // Validate input data (you can add more validation as needed)
+    if (!name || !description || !price) {
+      return res.status(400).json({ error: 'Name, description, and price are required' });
+    }
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      { name, description, price },
+      { new: true }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    res.json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+// Delete a product
+exports.deleteProduct = async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const deletedProduct = await Product.findByIdAndRemove(productId);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    res.json({ message: 'Product deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
