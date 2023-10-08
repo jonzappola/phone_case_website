891c89db58fdeb504ae977d966653a8cf1a39889
jonzappola <55258519+jonzappola@users.noreply.github.com>
2023-10-08 01:08:15 -0400

    Add files via upload

---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-content">
+        <p>&copy; 2023 TFM. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
